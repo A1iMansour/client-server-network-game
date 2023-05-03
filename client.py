d1d32899be7ac1f40e04cce149d6d44fdad6d63c
A1iMansour <ali.mansour5531@outlook.com>
2023-05-03 17:45:23 +0300

    clean

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import socket
 
-
+# This code was written by Christophe Kassab 
 # specify the server's IP address and port number
 SERVER_IP = '127.0.0.1'
-SERVER_PORT = 9589
+SERVER_PORT = 9989
 
 # create a TCP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,19 +26,18 @@ for round_num in range(3):
     number = input("Type the number and press Enter: ")
 
     # send the number to the server and record the start time
-
     client_socket.send(number.encode('ascii'))
-    print("sending done!")#just to debug
+
     # receive the round message from the server 
     result = client_socket.recv(1024)
     
 
     # display the result and the round trip time
-    print(f"Resukt: Round {round_num + 1} : {result.decode('ascii')}")
+    print(f"Result: Round {round_num + 1} : {result.decode('ascii')}")
     
     # receive and display the cumulative scores after each round
-    #cumulative_scores = client_socket.recv(1024)
-    #print(f"Cumulative scores after Round {round_num + 1}:\n{cumulative_scores.decode('ascii')}")
+    cumulative_scores = client_socket.recv(1024)
+    print(f"Cumulative scores after Round {round_num + 1}:\n{cumulative_scores.decode('ascii')}")
 
 #display the final result
 result = client_socket.recv(1024)
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import random
 import socket
 import socketserver
 import time
-
 import numpy as np
 
 """Remaining to do:
@@ -11,47 +10,42 @@ handle erors:
 2- if client or server didnt recieve answer
 3- if client did'nt answer after long duration
 ...
-Also,
-should score and cumulative score be RTT
-what is player was disqualified? 
-
-
 """
 
-
+# This part was written by Maya Chami
 host= '127.0.0.1'##IP address for local host.
-port=9589 ##Port number.
-
+port=9989 ##Port number.
 serv=socket.socket(socket.AF_INET,socket.SOCK_STREAM)##socket to accept connections.
-
 serv.bind((host,port))
 serv.listen()
+
 clients=[] #[(player1,0),(player2,1),(player3,2)]
 scores={0:0,1:0,2:0} #values are  score and keys are players ID
 roundnumber=0
 cumulative_score={0:0,1:0,2:0}# values are cumulative score and keys are players ID
 sortedcumulative_score={}
 sortedscores={}
- ##Incase some player left the game
 
-##Function to connect clients to server.
+# This part was written by Ali Mansour.
+#Function to connect clients to server.
 def connection():
     global sortedcumulative_score
     playernumber=1
     gamestarts=False
     while True:
-        print("while loop entered")
         if gamestarts==False:#to avoid waiting for clients in next rounds
             csocket,address=serv.accept()##communication socket,each connection gets its socket.
             csocket.sendall("Welcome to the ultimate gaming experience!".encode('ascii'))##sending welcome message.
             clients.append((csocket,f"Player{playernumber}"))
             playernumber+=1
 
-        print(f"while loop round number {roundnumber}")
-
         if len(clients)==3 and roundnumber<3:#Game acceptss 3 players, and three rounds
             gamestarts=True
             gamestarted()
+
+
+
+        # This part was written by Maya Chami
         elif roundnumber>=3:##GAME ended.
             Final_score=next(iter(sortedcumulative_score))
             winner=f"Winner Winner chicken Dinner: Player{Final_score} with score:{sortedcumulative_score[Final_score]},s"
@@ -63,7 +57,9 @@ def connection():
             csocket.close()
             break
 
-##Function that handles game's logic.
+
+# This part was written by Ali Mansour.
+#Function that handles game's logic.
 def gamestarted():
     global cumulative_score
     global scores
@@ -73,16 +69,13 @@ def gamestarted():
     roundnumber +=1
     randomnumber=random.randint(0,9)
     print(randomnumber)
-    # totalScores=[] # array to save cumulative scores
     maxrtt=0 # to be used for player who is diqualified
     disqualified=[]
     for i in range(len(clients)):
         print( f"player:{i}")
         timesend=time.time()##To calculate RTT.
         clients[i][0].sendall(f"{str(randomnumber)}".encode('ascii'))
-
         answer=clients[i][0].recv(1024).decode('ascii')##Recieving player's answer.
-
         ##+ add timer for player
         timereceived=time.time()
         print(f"time send:{timesend} , time recieved:{timereceived}")
@@ -93,19 +86,16 @@ def gamestarted():
             congrats = 'BRAVOO!!'
             clients[i][0].send(congrats.encode('ascii'))
             scores[i]= RTT
-            #totalScores.append((clients, RTT)) # add current round score to total score
             cumulative_score[i]+=scores[i] # add current round score to total score
         else:#wrong answer, Player disqualified from round. 
-
             disqualified.append(i)#store diqualified players
             disqualify_msg = 'You entered the wrong number and are disqualified from this round.'
             clients[i][0].send(disqualify_msg.encode('ascii'))
     
     for i in range(len(disqualified)):
-        scores[disqualified[i]]=maxrtt+1#disqualified player recieves highest RTT of current round + penalty
+        scores[disqualified[i]]=maxrtt+5#disqualified player recieves penalty: highest RTT of current round +  5
         cumulative_score[disqualified[i]]+=scores[disqualified[i]] # add current round score to total score
     # send results and cumulatives after each round
-    print(f'Results for round {roundnumber} :')
 
     #sorting cumulative_score in ascending order
     key=list(cumulative_score.keys())#create list of keys
@@ -119,17 +109,22 @@ def gamestarted():
     indicesofsortedvalues=np.argsort(score)#get indices of values in ascending order
     sortedscores={key[i]: score[i] for i in indicesofsortedvalues}#sort dictionary based on indices
     #descending order, highest score is player with min RTT
-    #printing in descending order this round's scores
+
+
+# This part was written by Maya Chami 
+    #printing in descending order this round's scores and sending them to clients
+    result=f'Results for round {roundnumber} :'+"\n"
     for j in sortedscores :
-        print('Round ', roundnumber, ': Player ', j, ':', scores[j], 'seconds')
+        result+='Round '+str(roundnumber)+  ': Player '+  str(j)+  ':'+  str(scores[j])+  ' seconds\n'
    
     #printing in descending order cumulative scores
     for j in sortedcumulative_score :
-        print('Overall Score', 'Player ',j,':',cumulative_score[j], 's')   
+        result+='Overall Score:  Round '+  str(roundnumber)+ ': Player '+ str(j)+ ':'+ str(scores[j])+ ' seconds\n'
 
-"""the results of the current round and the cumulative scores of
-each player in descending order must be displayed. After the three rounds, the
-server should declare the winner and close the connection."""
+    print(result)   
+    for i in range(len(clients)):
+        clients[i][0].sendall(result.encode('ascii'))
+
 
 ##Main function.
 def main():
